8a586ce85466629b85916f84b1695cd20653f787
Tuanny Ruiz <tuannyruiz@gmail.com>
2019-11-23 13:19:36 -0500

    add pop method

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -37,6 +37,7 @@ class SinglyLinkedList {
 
   push(val) {
     var newNode = new Node(val);
+
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
@@ -44,7 +45,9 @@ class SinglyLinkedList {
       this.tail.next = newNode;
       this.tail = newNode;
     }
+  
     this.length++;
+
     return this;
   }
 
@@ -55,10 +58,35 @@ class SinglyLinkedList {
   //     current = current.next;
   //   }
   // }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    var current = this.head;
+    var newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.lengt === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 let link = new SinglyLinkedList();
-console.log(link.push(1));
-console.log(link.push(10));
-console.log(link.push(15));
-console.log(link.traverse());
+link.push(1);
+link.push(10);
+link.push(15);
+console.log(link.pop());
