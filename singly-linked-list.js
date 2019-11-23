f66a3fcc8a7d153461c637165cf5a1beea6e32b3
Tuanny Ruiz <tuannyruiz@gmail.com>
2019-11-23 13:52:16 -0500

    bug fix

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -41,10 +41,10 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-
-    this.tail.next = newNode;
-    this.tail = newNode;
   
     this.length++;
 
@@ -108,10 +108,11 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
-    } 
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
 
-    newNode.next = this.head;
-    this.head = newNode;
     this.length++;
 
     return this;
