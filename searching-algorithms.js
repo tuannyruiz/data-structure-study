fa44c92379deba60ee248e1c75968acd4633fc55
Tuanny Ruiz <tuannyruiz@gmail.com>
2019-12-19 18:47:20 -0500

    Add binarySearch method

--- searching-algorithms.js
+++ searching-algorithms.js
@@ -8,6 +8,31 @@ function linearSearch (arr, val) {
     return -1;
 }
 
-let numbers = [1, 15, 4, 6, 74, 23];
-console.log(linearSearch(numbers, 55));
-console.log(linearSearch(numbers, 6));
+let linearNumbers = [1, 15, 4, 6, 74, 23];
+console.log(linearSearch(linearNumbers, 55));
+console.log(linearSearch(linearNumbers, 6));
+
+function binarySearch (arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((start + end)/2);
+
+    while (arr[middle] !== elem && start <= end) {
+        if (arr[middle] > elem) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end)/2);
+    }
+
+    if (arr[middle] === elem) {
+        return middle;
+    }
+
+    return -1;
+}
+
+let binaryNumbers = [1, 5, 6, 18, 22, 23, 60, 77, 82, 99];
+console.log(binarySearch(binaryNumbers, 60));
+console.log(binarySearch(binaryNumbers, 6));
