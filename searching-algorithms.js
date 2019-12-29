73b8a0d93c00790efc72faa7d33113b0d2a06e98
Tuanny Ruiz <tuannyruiz@gmail.com>
2019-12-29 18:47:16 -0500

    Add naiveString method

--- searching-algorithms.js
+++ searching-algorithms.js
@@ -29,3 +29,23 @@ function binarySearch (arr, elem) {
 
     return -1;
 }
+
+
+function naiveString (long, short) {
+    let count = 0;
+    
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i+j]) {
+                break;
+            }
+            if (j === short.length - 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+console.log(naiveString("eu breu teu", "eu"));
